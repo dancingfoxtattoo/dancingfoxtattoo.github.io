bf9c4ff2a7c88287a567efd1339c582d988f0cd9
Merlin (they/them) <421960+merlinpatt@users.noreply.github.com>
2022-09-17 21:06:46 -0400

    Add booking toggle

---
+++ imageData.example.js
@@ -0,0 +1,18 @@
+const imageDataExample = {
+  caption: '#tag1 text #tag2 #tag3',
+  id: '',
+  image: '', // long URL
+  link: '', // short URL
+  model: {
+    id: '',
+    media_type: 'IMAGE',
+    media_url: '', // medium URL
+    permalink: '', // short URL, same as link
+    timestamp: '2022-09-05T02:13:56+0000',
+    username: ''
+  },
+  tags: ['tag1', 'tag2', 'tag3'],
+  timestamp: "2022-09-05T02:13:56+0000",
+  type: "image",
+  username: ''
+};
